b62d43d75b1acbe5b603fbb0b1d7184a593759bd
Rameez <rameesmohd789@gmail.com>
2024-06-02 03:25:49 +0530

    nav home

--- src/components/User/Nav.jsx
+++ src/components/User/Nav.jsx
@@ -5,7 +5,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import logo from '../../assets/Beat (3) copy.png'
 import { logout } from '../../Redux/UserSlice';
 
-const Navbar = () => {
+const app = () => {
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
     const token = useSelector((state)=>state.Client.token)
     const dispatch = useDispatch()
@@ -91,4 +91,4 @@ const Navbar = () => {
   )
 }
 
-export default Navbar
+export default app
